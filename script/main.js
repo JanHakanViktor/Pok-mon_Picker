6aad381903893f50e6adb6e8618a6debd9f8e821
JanHakanViktor <janhakanv@gmail.com>
2024-11-07 22:06:30 +0100

    added character selection functionality and styling

--- script/main.js
+++ script/main.js
@@ -1,68 +1,43 @@
 // kör main-funktionen när DOM är laddad
 window.addEventListener("DOMContentLoaded", main);
 
+const ash = document.querySelector("#ash img");
+const gary = document.querySelector("#gary img");
+const description = document.querySelector("#description");
 
 function main() {
-    loadStartingScene()
+    toggleAsh();
+    toggleGary();
 }
 
-function loadStartingScene(){
-    const p=document.createElement("p");
-    document.body.append(p);
+function toggleAsh() {
+    
+    ash.onclick = function(){
+        const name = "Ash Ketchum";
+        ash.classList.toggle("selected");
+        gary.classList.remove("selected");
+        if(ash.classList.contains("selected")){
+            description.innerHTML = "Du har valt " + name + ", dags att påbörja din resa!";
+        } else{
+            description.innerHTML = "Välj din karaktär och påbörja din resa";
+        }
+    }
 }
 
-function loadEnterLivingRoom(){
-
+function toggleGary() {
+    
+    gary.onclick = function(){
+        const name = "Gary Oak";
+        gary.classList.toggle("selected");
+        ash.classList.remove("selected");
+        if(gary.classList.contains("selected")){
+            description.innerHTML = "Du har valt " + name + ", dags att påbörja din resa!";
+        } else{
+            description.innerHTML = "Välj din karaktär och påbörja din resa";
+        }
+    }
 }
 
-function goBackToSleep(){
-    //ladda svart bakgrund som cover allt och lägg till en knapp "Vakna upp igen" som startar om första scenen
+function nextPage() {
+    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// globalt tillstånd för vår applikation, läggs oftast övre delen av scripts. Inte allt men många. 
-// const todoList = ["Städa", "Handla godis"];
-
-// saveTodoButton.onclick = saveTodo;
-
-
-// här startar applikationen
-// function saveTodo() {
-//     todoList.push(todoInput.value)
-
-//     console.log(todoList);
-// }
-
-
-
-
-// var value = 0;
-// while(value < 100) {
-// 	console.log(value);
-// 	value++;
-// }
-
-
-
-
-// let value = 0;
-
-// console.time("time");
-
-// while (value < 100_000) {
-//     value +=1;
-// }
-
-// console.timeEnd("time");
