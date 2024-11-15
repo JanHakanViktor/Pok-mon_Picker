94ee51ce440f43675e8625ec1f5acd476fc5927d
JanHakanViktor <janhakanv@gmail.com>
2024-11-15 18:36:24 +0100

    hopefully fixed pathing to housePage

--- script/main.js
+++ script/main.js
@@ -192,7 +192,7 @@ function housePage(){
 
     const gameSceneOne = document.createElement("img");
     gameSceneOne.className = "videoStyling";
-    gameSceneOne.src = "./assets/HouseRoom.png";
+    gameSceneOne.src = "./assets/houseRoom.png";
 
     const buttonBack = document.createElement("button"); 
     buttonBack.textContent = "Tillbaka";
